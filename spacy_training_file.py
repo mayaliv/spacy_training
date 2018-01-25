64c9c092942d0075ab9841dc0b37d805481a1da0
Maya Livshits <mayaglas+git@gmail.com>
2018-01-25 13:34:59 -0800

    This code is for training named entities for Spacy based on trained model. The code uses "text.txt" file to extract entities to test the model before and after the training. the code uses the "train_data.txt" in JSON format to train the model.

--- spacy_training_file.py
+++ spacy_training_file.py
@@ -1,69 +1,63 @@
 from __future__ import unicode_literals, print_function
 
-import plac
+import json
+from pprint import pprint
 import spacy
 import random
 from pathlib import Path
+from datetime import datetime
+
+start_time = datetime.now()
+
+#choose a model
+model='en_core_web_sm' # model package "en_core_web_sm"
+#model='en_core_web_md' # model package "en_core_web_md"
+#model='en_core_web_lg' # model package "en_core_web_lg"
+
+#path for directory to save the model
+output_dir='/Users/maya/PycharmProjects/spacy_training/en_example_model'
+
+#input files
+text_file="text.txt"
+train_file="train_data.txt"
+
+nlp = spacy.load(model)           # load model package
+
+#load text file, read and extract named entities
+f= open(text_file,'r')
+
+texts=f.read().decode('utf-8').splitlines()
+for text in texts:
+    print('\n',text)
+    doc = nlp(text)
+
+    for ent in doc.ents:
+        print(ent.text, ent.start_char, ent.end_char, ent.label_)
 
 
-TEXTS = [
-    'Who is Shaka Khan?',
-    'I like London and Berlin.',
-]
-
-nlp = spacy.load('en_core_web_sm')           # load model package "en_core_web_sm"
-
-doc1 = nlp(u'Apple is looking at buying U.K. startup for $1 billion')
-
-for ent in doc1.ents:
-    print(ent.text, ent.start_char, ent.end_char, ent.label_)
-
-doc2 = nlp(u'Who is Shaka Khan?')
-
-for ent in doc2.ents:
-     print(ent.text, ent.start_char, ent.end_char, ent.label_)
-
-doc3 = nlp(u'I like London and Berlin')
-
-for ent in doc3.ents:
-     print(ent.text, ent.start_char, ent.end_char, ent.label_)
+f.close()
 
 
-# !/usr/bin/env python
 # coding: utf8
-"""Example of training spaCy's named entity recognizer, starting off with an
-existing model or a blank model.
+"""training spaCy's named entity recognizer, starting off with an
+existing model.
 For more details, see the documentation:
 * Training: https://spacy.io/usage/training
 * NER: https://spacy.io/usage/linguistic-features#named-entities
     Compatible with: spaCy v2.0.0+
     """
 
-# a change
+#read training file from JSON format
+f= open(train_file,'r')
 
-# training data
-TRAIN_DATA = [
-    ('Who is Shaka Khan?', {
-        'entities': [(7, 17, 'PERSON')]
-    }),
-    ('I like London and Berlin.', {
-        'entities': [(7, 13, 'LOC'), (18, 24, 'LOC')]
-    })
-]
+TRAIN_DATA = json.load(open(train_file))
+#pprint(TRAIN_DATA)
 
-
-@plac.annotations(
-    model=("Model name. Defaults to blank 'en' model.", "option", "m", str),
-    output_dir=("Optional output directory", "option", "o", Path),
-    n_iter=("Number of training iterations", "option", "n", int))
-def main2(model=None, output_dir=None, n_iter=100):
+def main(model=None, output_dir=None, n_iter=100):
     """Load the model, set up the pipeline and train the entity recognizer."""
-    if True: #model is not None:
-        nlp = spacy.load('en_core_web_sm')  # load existing spaCy model
-        print("Loaded model '%s'" % model)
-    else:
-        nlp = spacy.blank('en')  # create blank Language class
-        print("Created blank 'en' model")
+
+    nlp = spacy.load(model)  # load existing spaCy model
+    print('\n',"Loaded model '%s'" % model)
 
     # create the built-in pipeline components and add them to the pipeline
     # nlp.create_pipe works for built-ins that are registered with spaCy
@@ -93,7 +87,7 @@ def main2(model=None, output_dir=None, n_iter=100):
                     drop=0.5,  # dropout - make it harder to memorise data
                     sgd=optimizer,  # callable to update weights
                     losses=losses)
-            print(losses)
+            #print(losses)
 
     # test the trained model
     for text, _ in TRAIN_DATA:
@@ -101,7 +95,7 @@ def main2(model=None, output_dir=None, n_iter=100):
         print('Entities', [(ent.text, ent.label_) for ent in doc.ents])
         print('Tokens', [(t.text, t.ent_type_, t.ent_iob) for t in doc])
 
-    output_dir='/Users/maya/PycharmProjects/spacy_training/en_example_model'
+
 
     # save model to output directory
     if output_dir is not None:
@@ -109,10 +103,10 @@ def main2(model=None, output_dir=None, n_iter=100):
         if not output_dir.exists():
             output_dir.mkdir()
         nlp.to_disk(output_dir)
-        print("Saved model to", output_dir)
+        print('\n',"Saved model to", output_dir)
 
         # test the saved model
-        print("Loading from", output_dir)
+        print("Test, Loading from", output_dir)
         nlp2 = spacy.load(output_dir)
         for text, _ in TRAIN_DATA:
             doc = nlp2(text)
@@ -120,29 +114,20 @@ def main2(model=None, output_dir=None, n_iter=100):
             print('Tokens', [(t.text, t.ent_type_, t.ent_iob) for t in doc])
 
 
-if __name__ == '__main__':
-    plac.call(main2)
-
-
-
-
-
-
-
+main(model,output_dir)
+#load the saved model
 nlp = spacy.load('/Users/maya/PycharmProjects/spacy_training/en_example_model')
 
-doc4 = nlp(u'Who is Shaka Khan?')
+print('\n',"Testing trained and untrained data")
 
-for ent in doc4.ents:
-     print(ent.text, ent.start_char, ent.end_char, ent.label_)
+#Testing both trained data and untrained data
+for text in texts:
+    print('\n',text)
+    doc = nlp(text)
 
-doc5 = nlp(u'I like London and Berlin')
+    for ent in doc.ents:
+        print(ent.text, ent.start_char, ent.end_char, ent.label_)
 
-for ent in doc5.ents:
-     print(ent.text, ent.start_char, ent.end_char, ent.label_)
+time_elapsed = datetime.now() - start_time
 
-
-doc6 = nlp(u'Apple is looking at buying U.K. startup for $1 billion')
-
-for ent in doc6.ents:
-    print(ent.text, ent.start_char, ent.end_char, ent.label_)
+print('Time elapsed (hh:mm:ss.ms) {}'.format(time_elapsed))
